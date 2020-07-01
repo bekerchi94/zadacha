f90b09693bba1e4b9a9b312a42dde68afd60947a
bekerchi94 <59106624+bekerchi94@users.noreply.github.com>
2020-07-01 19:03:28 +0600

    view

--- view/vadmin.php
+++ view/vadmin.php
@@ -1,4 +1,4 @@
-﻿<h1>Список задач</h1>
+<h1>Список задач</h1>
 
 <div class="row">
 	<div class="col-1">
@@ -22,11 +22,13 @@
 
 				<? if($spisok_c>1){ ?>
 				<li class="page-item">.....</li>
+				<? //for($i=1;$i<=$spisok_c;$i++){ ?>
 				<li class="page-item">
 					<a class="page-link" href="#">
 						<?= $pg; ?>
 					</a>
 				</li>
+				<? //} ?>
 				<li class="page-item">.....</li>
 				<li class="page-item">
 					<a class="page-link" href="./?sp&page=<?= $spisok_c; ?>">
@@ -75,7 +77,8 @@
 		<div class="col-12">
 
 			<div class="row">
-				<div class="col-4">
+
+				<div class="col-3">
 					<div class="row">
 						<div class="col-4">
 Пользователь: 
@@ -85,8 +88,8 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-4">
 
+				<div class="col-3">
 					<div class="row">
 						<div class="col-4">
 e-mail: 
@@ -95,52 +98,52 @@ e-mail:
 							<?= $sp[2]; ?>
 						</div>
 					</div>
-
 				</div>
-				<div class="col-4">
 
+				<div class="col-3">
 					<div class="row">
 						<div class="col-2">
 статус: 
 						</div>
 						<div class="col-6">
-							<? if($sp[4]=='0'){echo "еще не выполнено"?>
-							<img width="20px" height="20px" src="./nep.png" />
-							<? } ?>
-							<? if($sp[4]=='1'){echo "выполнено"?>
-							<img width="20px" height="20px" src="./vyp.png" />
-							<? } ?>
-						</div>
-						<div class="col-4">
-							<? if($sp[5]==1){ ?>
+
+							<div class="form-check form-check-inline">
+								<? if($sp[4]=='0'){ ?>
+								<input class="form-check-input" type="checkbox" name="setch" id="setch<?= $sp[0]; ?>"  onclick="setstat(<?= $sp[0]; ?>);" value="<?= $sp[0]; ?>">
+									<label class="form-check-label" id="setchl<?= $sp[0]; ?>" >еще не выполнено</label>
+									<? } ?> 
+									<? if($sp[4]=='1'){ ?>
+									<input class="form-check-input" type="checkbox" name="setch" checked id="setch<?= $sp[0]; ?>" onclick="setstat(<?= $sp[0]; ?>);" value="<?= $sp[0]; ?>">
+										<label class="form-check-label" id="setchl<?= $sp[0]; ?>" >выполнено</label>
+										<? } ?> 
+									</div>
+								</div>
+								<div class="col-4" id="red<?= $sp[0]; ?>">
+									<? if($sp[5]==1){ ?>
 отредактировано администратором
-							<? } ?>
+									<? } ?>
+								</div>
+
+							</div>
+						</div>
+						<div class="col-3">
+							<button class="btn btn-primary btn-block" onclick="edid(<?= $sp[0]; ?>);" id="b<?= $sp[0]; ?>" str="r" >Редактировать</button>
+						</div>
+					</div>
+
+
+					<hr>
+						<div class="row">
+							<div class="col-12">
+Задача: 
+							</div>
+							<div class="col-12 mytextarea" id="ta<?= $sp[0]; ?>">
+								<?= $sp[3]; ?>
+							</div>
+
 						</div>
 
-
 					</div>
-
-
-
 				</div>
-
-			</div>
-
-
-			<hr>
-				<div class="row">
-					<div class="col-12">
-Задача: 
-					</div>
-					<div class="col-12">
-						<?= $sp[3]; ?>
-					</div>
-
-				</div>
-
-			</div>
-		</div>
-		<hr>
-			<? } ?>
-
-			
+				<hr>
+					<? } ?>
--- view/vdobavit.php
+++ view/vdobavit.php
@@ -1,8 +1,7 @@
-<?php  include(dirname(__DIR__, 1).'/contoller/mdobavit.php'); ?> 
 <h1>Добавить задачу</h1>
 
-<div class="alert alert-primary" role="alert">
-	<?php echo $resul; ?>
+<div class="alert alert-danger" id="verr" role="alert">
+	<?= $resul; ?>
 </div>
 
 <form id="form1" class="form-horizontal" action="./?db" method="post">
@@ -36,55 +35,8 @@
 		</div>
 	</div>
 	<div class="col-8">
-		<input type="button" class="btn btn-primary btn-block" onclick="tek();" value="Сохранить" />
+		<input type="button" class="btn btn-primary btn-block" id="sohr"  value="Сохранить" />
 	</div>
 
 </form>
 
-<script>
-
-$(document).ready(function() {
-    $('#form1').bootstrapValidator({
-        feedbackIcons: {
-            valid: 'glyphicon glyphicon-ok',
-            invalid: 'glyphicon glyphicon-remove',
-            validating: 'glyphicon glyphicon-refresh'
-        },
-        fields: {
-            email: {
-                validators: {
-                    emailAddress: {
-                        message: 'неправильный адрес электронной почты'
-                    }
-                }
-            }
-        }
-    });
-});
-
-
-function tek()
-{
-	
-var name=document.getElementById('name').value;
-var email=document.getElementById('emaill').value;	
-var text=document.getElementById('textl').value;	
-if((name.length==0)||(email.length==0)||(text.length==0))
-{
- alert("Ошибка!!!      Заполните все поля!");	
-}else
-{
-	document.getElementById('form1').submit();
-}
-}
-
-
-
-
-
-
-
-
-
-
-</script>
